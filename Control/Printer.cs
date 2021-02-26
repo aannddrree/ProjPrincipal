01ba90fc1ced1e5b3b184dedfe43e93bcd2260b1
unknown <aannddrree>
2021-02-26 13:47:53 -0300

    Implantação inicial do projeto

---
+++ Control/Printer.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public class Printer
+    {
+        public static string GetPerson(Pessoa pessoa)
+        {
+            return "Id:" + pessoa.Id + " Nome: " + pessoa.Nome;
+        }
+    }
+}
---
+++ ProjPrincipal/Program.cs
@@ -0,0 +1,36 @@
+﻿using Control;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario funcionario = new Funcionario()
+            {
+                Id = 144,
+                Nome = "José",
+                Telefone = "16 9 88778778"
+            };
+
+            Cliente cliente = new Cliente()
+            {
+                Id = 1,
+                Nome = "Luiz",
+                Telefone = "16 9 88774545"
+            };
+
+            Console.WriteLine(Printer.GetPerson(funcionario));
+            Console.WriteLine(Printer.GetPerson(cliente));
+
+            Console.ReadKey();
+        }
+    }
+}
